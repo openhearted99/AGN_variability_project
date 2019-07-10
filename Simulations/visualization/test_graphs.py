3f86edc1b0b598dd0c640d3e1adbd197c4b1bb04
Theodore Pena <theodore.pena@tufts.edu>
2019-07-10 15:55:42 -0400

    Phase 2 begins in earnest.

---
+++ Simulations/visualization/test_graphs.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+default_curves = []
+for i in range(20):
+    print('/home/tpena/AGN_variability_project/Simulations/light_curves/default_params/results_Default_curve_{}.bin'.format(str(i)) )
+
+    
+    
