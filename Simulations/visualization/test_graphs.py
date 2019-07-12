67674df3fe6b92f1dee2a20b5cc2d52a93bde728
Theodore Pena <theodore.pena@tufts.edu>
2019-07-12 09:54:59 -0400

    New plots! More simulation to come very soon

--- Simulations/visualization/test_graphs.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-default_curves = []
-for i in range(20):
-    print('/home/tpena/AGN_variability_project/Simulations/light_curves/default_params/results_Default_curve_{}.bin'.format(str(i)) )
-
-    
-    
