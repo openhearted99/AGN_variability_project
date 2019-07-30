ae4d610086f18e63105ef9c7486539b4a81772ab
Theodore Pena <theodore.pena@tufts.edu>
2019-07-30 15:06:34 -0400

    Added some new arrays

--- Simulations/visualization/Factory3.0.py
+++ Simulations/visualization/Factory3.0.py
@@ -4,16 +4,14 @@ Author: Theodore Pena
 Contact: theodore.pena@tufts.edu
 """
 
-import sys
-
-import matplotlib
-import matplotlib.pyplot as plt
-
-import numpy as np
-
-import tqdm as tqdm
-
 import random
+import tqdm as tqdm
+import numpy as np
+from scipy.stats import chisquare
+import matplotlib.pyplot as plt
+import sys
+from sklearn.metrics.pairwise import euclidean_distances
+import matplotlib
 random.seed()
 
 
@@ -24,24 +22,30 @@ if __name__ == "__main__":
 def load_data():
     pass
 
+
 def cut_last():
     pass
+
 
 def get_indicies():
     pass
 
+
 def get_smallest_point():
     pass
+
 
 def select_points():
     pass
 
+
 def analyze_data():
     pass
+
 
 def graph_trends():
     pass
 
+
 def find_delta():
     pass
-
